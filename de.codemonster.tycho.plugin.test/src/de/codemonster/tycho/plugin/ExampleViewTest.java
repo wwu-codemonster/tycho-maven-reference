960184ce429dc7e4108c97a6c03c260688c46464
Jannis Brune <jannis.brune@hs-osnabrueck.de>
2020-09-05 16:08:01 +0200

    Added finished project

---
+++ de.codemonster.tycho.plugin.test/src/de/codemonster/tycho/plugin/ExampleViewTest.java
@@ -0,0 +1,13 @@
+package de.codemonster.tycho.plugin;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class ExampleViewTest {
+
+	 @Test
+	 public void testID() {
+	  assertEquals("de.codemonster.tycho.views.example", ExampleView.VIEW_ID);
+	 }
+}
